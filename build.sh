bf04aa5e81c42175fbac4f31deb2a83dd81721a8
tukiyo3 <tukiyo3@gmail.com>
2017-11-05 09:14:40 +0900

    gen_lex_hash.cc.patch

--- build.sh
+++ build.sh
@@ -2,7 +2,8 @@ export BUILD="docker build . --no-cache=false -f "
 
 #$BUILD Dockerfile.centos5 -t tukiyo/mysql4php52:centos5
 #$BUILD Dockerfile.centos6 -t tukiyo/mysql4php52:centos6
-$BUILD Dockerfile.centos7 -t tukiyo/mysql4php52:centos7
+#$BUILD Dockerfile.centos7 -t tukiyo/mysql4php52:centos7
+$BUILD Dockerfile.centos7 -t tukiyo/mysql4php52:centos7mysqld
 #$BUILD Dockerfile.fc25 -t tukiyo/mysql4php52:fc25
 #$BUILD Dockerfile.ubuntu1604 -t tukiyo/mysql4php52:ubuntu1604
 #$BUILD Dockerfile.ubuntu1610 -t tukiyo/mysql4php52:ubuntu1610
