c039a788a5661351214218d47b888660b0a4de21
tukiyo3 <tukiyo3@gmail.com>
2019-10-29 13:07:19 +0900

    ruby 1.8.7 の make test が CentOS 7 でsuccessになることを再確認

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
-IMAGE="tukiyo/mysql4php52:centos7"
 IMAGE="tukiyo/mysql4php52:debian10"
+IMAGE="tukiyo/mysql4php52:centos8"
+IMAGE="tukiyo/mysql4php52:centos7"
 docker run --rm -v `pwd`:/host/  -it $IMAGE /bin/bash
