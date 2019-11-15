55c024dd6008af92b5b5f7be5910a6303f3bbbce
tukiyo3 <tukiyo3@gmail.com>
2019-11-15 10:13:16 +0900

    update

--- run.sh
+++ run.sh
@@ -1,6 +1,4 @@
 IMAGE="tukiyo/mysql4php52:debian10"
 IMAGE="tukiyo/mysql4php52:centos8"
 IMAGE="tukiyo/mysql4php52:centos7"
-IMAGE="tukiyo/mysql4php52:centos8-ruby"
-IMAGE="tukiyo/mysql4php52:centos8-gcc447"
 docker run --rm -v `pwd`:/host/  -it $IMAGE /bin/bash
