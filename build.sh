fed83debd0a38e1586a46938db38ce5211601973
tukiyo3 <tukiyo3@gmail.com>
2019-11-18 09:07:47 +0900

    add: compat-gcc

--- build.sh
+++ build.sh
@@ -2,8 +2,8 @@ export BUILD="docker build . --no-cache=false -f "
 
 #$BUILD Dockerfile.centos5 -t tukiyo/mysql4php52:centos5
 #$BUILD Dockerfile.centos6 -t tukiyo/mysql4php52:centos6
-$BUILD Dockerfile.centos7 -t tukiyo/mysql4php52:centos7
-#$BUILD Dockerfile.centos8 -t tukiyo/mysql4php52:centos8
+#$BUILD Dockerfile.centos7 -t tukiyo/mysql4php52:centos7
+$BUILD Dockerfile.centos8 -t tukiyo/mysql4php52:centos8
 
 #$BUILD Dockerfile.fc25 -t tukiyo/mysql4php52:fc25
 
