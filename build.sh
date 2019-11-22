453395c95f1d1ffe25cc1a50d820f4fb64aad980
tukiyo3 <tukiyo3@gmail.com>
2019-11-22 09:35:06 +0900

    add: ubuntu20.04

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ export BUILD="docker build . --no-cache=false -f "
 #$BUILD Dockerfile.centos6 -t tukiyo/mysql4php52:centos6
 #$BUILD Dockerfile.centos7 -t tukiyo/mysql4php52:centos7
 #$BUILD Dockerfile.centos8 -t tukiyo/mysql4php52:centos8
-$BUILD Dockerfile.centos8-mariadb55 -t tukiyo/mysql4php52:centos8-mariadb55
+#$BUILD Dockerfile.centos8-mariadb55 -t tukiyo/mysql4php52:centos8-mariadb55
 
 #$BUILD Dockerfile.fc25 -t tukiyo/mysql4php52:fc25
 
@@ -13,6 +13,7 @@ $BUILD Dockerfile.centos8-mariadb55 -t tukiyo/mysql4php52:centos8-mariadb55
 #$BUILD Dockerfile.ubuntu1704 -t tukiyo/mysql4php52:ubuntu1704
 #$BUILD Dockerfile.ubuntu1804 -t tukiyo/mysql4php52:ubuntu1804
 #$BUILD Dockerfile.ubuntu1910 -t tukiyo/mysql4php52:ubuntu1910
+$BUILD Dockerfile.ubuntu2004 -t tukiyo/mysql4php52:ubuntu2004
 
 #$BUILD Dockerfile.debian9 -t tukiyo/mysql4php52:debian9
 #$BUILD Dockerfile.debian10 -t tukiyo/mysql4php52:debian10
--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 IMAGE="tukiyo/mysql4php52:debian10"
 IMAGE="tukiyo/mysql4php52:centos7"
 IMAGE="tukiyo/mysql4php52:centos8"
+IMAGE="tukiyo/mysql4php52:ubuntu2004"
 docker run --rm -v `pwd`:/host/  -it $IMAGE /bin/bash
