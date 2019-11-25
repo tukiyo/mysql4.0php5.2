484afd87ad11bc44a86032cd4d2f8adb57df6ad6
tukiyo3 <tukiyo3@gmail.com>
2019-11-25 10:25:01 +0900

    update

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@ export BUILD="docker build . --no-cache=false -f "
 
 #$BUILD Dockerfile.centos5 -t tukiyo/mysql4php52:centos5
 #$BUILD Dockerfile.centos6 -t tukiyo/mysql4php52:centos6
-#$BUILD Dockerfile.centos7 -t tukiyo/mysql4php52:centos7
+$BUILD Dockerfile.centos7 -t tukiyo/mysql4php52:centos7
 #$BUILD Dockerfile.centos8 -t tukiyo/mysql4php52:centos8
 #$BUILD Dockerfile.centos8-mariadb55 -t tukiyo/mysql4php52:centos8-mariadb55
 
@@ -11,7 +11,7 @@ export BUILD="docker build . --no-cache=false -f "
 #$BUILD Dockerfile.ubuntu1604 -t tukiyo/mysql4php52:ubuntu1604
 #$BUILD Dockerfile.ubuntu1610 -t tukiyo/mysql4php52:ubuntu1610
 #$BUILD Dockerfile.ubuntu1704 -t tukiyo/mysql4php52:ubuntu1704
-$BUILD Dockerfile.ubuntu1804 -t tukiyo/mysql4php52:ubuntu1804
+#$BUILD Dockerfile.ubuntu1804 -t tukiyo/mysql4php52:ubuntu1804
 #$BUILD Dockerfile.ubuntu1910 -t tukiyo/mysql4php52:ubuntu1910
 #$BUILD Dockerfile.ubuntu2004 -t tukiyo/mysql4php52:ubuntu2004
 
