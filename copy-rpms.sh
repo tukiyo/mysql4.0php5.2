de6155fd1c8aa51f3204f577513ecb814f085f39
tukiyo3 <tukiyo3@gmail.com>
2019-12-03 14:12:47 +0900

    update

--- copy-rpms.sh
+++ copy-rpms.sh
@@ -1,6 +1,6 @@
 mkdir -p /host/tmp/
 
 cd /
-cp $(find / -type f -name "*\.rpm" | grep -v host | grep -v src.rpm) /host/tmp/
+cp $(find / -type f -name "*\.rpm" | egrep -v "/host/|.src.rpm|checkinstall") /host/tmp/
 
 chmod 777 /host/tmp/
