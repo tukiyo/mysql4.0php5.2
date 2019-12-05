57d15411419ddb3c554a087df08ba79e4c9515bd
tukiyo3 <tukiyo3@gmail.com>
2019-12-05 10:40:37 +0900

    update

--- build.sh
+++ build.sh
@@ -1,15 +1,14 @@
 export BUILD="docker build . --no-cache=false -f "
 
-# $BUILD dockerfile/Dockerfile.centos5 -t tukiyo/mysql4php52:centos5
-# $BUILD dockerfile/Dockerfile.centos6 -t tukiyo/mysql4php52:centos6
-# $BUILD dockerfile/Dockerfile.centos7-mysql40 -t tukiyo/mysql4php52:centos7-mysql40
-# $BUILD dockerfile/Dockerfile.centos7-20191204 -t tukiyo/mysql4php52:centos7-20191204
-# $BUILD dockerfile/Dockerfile.centos8 -t tukiyo/mysql4php52:centos8
-# $BUILD dockerfile/Dockerfile -t tukiyo/mysql4php52
+# $BUILD dockerfiles/Dockerfile.centos5 -t tukiyo/mysql4php52:centos5
+# $BUILD dockerfiles/Dockerfile.centos6 -t tukiyo/mysql4php52:centos6
+# $BUILD dockerfiles/Dockerfile.centos7-mysql40 -t tukiyo/mysql4php52:centos7-mysql40
+# $BUILD dockerfiles/Dockerfile.centos7-20191204 -t tukiyo/mysql4php52:centos7-20191204
+# $BUILD dockerfiles/Dockerfile.centos8 -t tukiyo/mysql4php52:centos8
+# $BUILD dockerfiles/Dockerfile -t tukiyo/mysql4php52
 
-# $BUILD dockerfile/Dockerfile.fc25 -t tukiyo/mysql4php52:fc25
+$BUILD dockerfiles/Dockerfile.ubuntu2004-build -t tukiyo3/ubuntu-build:20.04
+$BUILD dockerfiles/Dockerfile.ubuntu2004 -t tukiyo/mysql4php52:ubuntu2004
+# $BUILD dockerfiles/Dockerfile.ubuntu2004-sqlbench -t tukiyo/mysql4php52:ubuntu2004-sqlbench
 
-$BUILD dockerfile/Dockerfile.ubuntu2004 -t tukiyo/mysql4php52:ubuntu2004
-# $BUILD dockerfile/Dockerfile.ubuntu2004-sqlbench -t tukiyo/mysql4php52:ubuntu2004-sqlbench
-
-# $BUILD dockerfile/Dockerfile.debian10 -t tukiyo/mysql4php52:debian10
+# $BUILD dockerfiles/Dockerfile.debian10 -t tukiyo/mysql4php52:debian10
