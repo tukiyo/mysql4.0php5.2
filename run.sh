801ad88f236a7b2f8e88dbe59d2ae4b4fb7cbf9e
tukiyo3 <tukiyo3@gmail.com>
2019-12-13 08:02:14 +0900

    update

--- run.sh
+++ run.sh
@@ -1,14 +1,18 @@
 #!/bin/sh
 
 IMAGE="centos:6"
-IMAGE="centos:8"
-IMAGE="tukiyo/mysql4php52:centos7-2019-1126"
 IMAGE="tukiyo/mysql4php52:centos8"
 IMAGE="tukiyo/mysql4php52:debian10"
 IMAGE="tukiyo/mysql4php52:latest"
-IMAGE="tukiyo3/centos7-build"
 IMAGE="ubuntu:20.04"
 IMAGE="tukiyo/mysql4php52:ubuntu2004"
 IMAGE="tukiyo/mysql4php52:ubuntu2004-sqlbench"
+IMAGE="centos:8"
+IMAGE="tukiyo3/centos8-build"
+IMAGE="tukiyo3/centos7-build"
+
+IMAGE="tukiyo/mysql4php52:centos8-mysql5openssl102ruby18tenshi"
+IMAGE="tukiyo/mysql4php52:centos7-20191213"
+IMAGE="tukiyo/mysql4php52:centos8-mysql4php52"
 
 docker run --rm -v `pwd`:/host/  -it $IMAGE /bin/bash
