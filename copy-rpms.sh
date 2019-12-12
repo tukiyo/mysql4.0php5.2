8a84a3392bdf7db6d76cc00d15ba5ec4980a17f6
tukiyo3 <tukiyo3@gmail.com>
2019-12-12 19:45:59 +0900

    update

--- copy-rpms.sh
+++ copy-rpms.sh
@@ -1,11 +1,14 @@
-mkdir -p /host/{rpm,deb}
+#!/bin/sh
 
 cd /
-cp $(find / -type f -name "*\.rpm" | egrep -v "/host/|.src.rpm|checkinstall") /host/rpm/
-cp $(find / -type f -name "*\.deb" | egrep -v "/host/|.src.rpm|checkinstall") /host/deb/
+
+# rpmのコピー
+(find / -type f -name "*\.rpm" | egrep -v "/host/|.src.rpm|checkinstall") \
+&& mkdir -p /host/rpm \
+&& cp $(find / -type f -name "*\.rpm" | egrep -v "/host/|.src.rpm|checkinstall") /host/rpm/
+
+(find / -type f -name "*\.deb" | egrep -v "/host/") \
+&& mkdir -p /host/deb \
+&& cp $(find / -type f -name "*\.deb" | egrep -v "/host/") /host/deb/
 
 chmod 777 /host/{rpm,deb}
-
-# list
-find / -type f -name "*\.rpm" | egrep -v "/host/|.src.rpm|checkinstall"
-find / -type f -name "*\.deb" | egrep -v "/host/|.src.rpm|checkinstall"
