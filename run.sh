ccc877ef7b005c2363244bc4ec71f27f03473c97
tukiyo3 <tukiyo3@gmail.com>
2020-08-15 01:37:12 +0900

    update

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 IMAGE="tukiyo3/mysql4php52:centos8-mysql5openssl102ruby18tenshi"
 #IMAGE="tukiyo3/mysql4php52:centos8-mysql4php52"
 IMAGE="tukiyo3/mysql4php52:centos8-gd.so"
-#IMAGE="tukiyo3/mysql4php52:centos7-20200427"
+#IMAGE="tukiyo3/mysql4php52:centos7"
 #IMAGE="tukiyo3/mysql4php52:centos7-mysql40"
 #IMAGE="tukiyo3/mysql4php52:ubuntu2004"
 #IMAGE="tukiyo3/mysql4php52:debian10"
