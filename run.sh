9c36d5215d78b0c9d8200e82c2008462d2b68082
tukiyo3 <tukiyo3@gmail.com>
2020-08-15 01:03:17 +0900

    checkinstallオプションを見直した

--- run.sh
+++ run.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
-IMAGE="tukiyo/mysql4php52:centos8-mysql5openssl102ruby18tenshi"
-#IMAGE="tukiyo/mysql4php52:centos8-mysql4php52"
-IMAGE="tukiyo/mysql4php52:centos8-gd.so"
-#IMAGE="tukiyo/mysql4php52:centos7-20200427"
-#IMAGE="tukiyo/mysql4php52:centos7-mysql40"
-#IMAGE="tukiyo/mysql4php52:ubuntu2004"
-#IMAGE="tukiyo/mysql4php52:debian10"
+IMAGE="tukiyo3/mysql4php52:centos8-mysql5openssl102ruby18tenshi"
+#IMAGE="tukiyo3/mysql4php52:centos8-mysql4php52"
+IMAGE="tukiyo3/mysql4php52:centos8-gd.so"
+#IMAGE="tukiyo3/mysql4php52:centos7-20200427"
+#IMAGE="tukiyo3/mysql4php52:centos7-mysql40"
+#IMAGE="tukiyo3/mysql4php52:ubuntu2004"
+#IMAGE="tukiyo3/mysql4php52:debian10"
+IMAGE="tukiyo3/mysql4php52:centos6"
 
 docker run --rm -v `pwd`:/host/  -it $IMAGE /bin/bash
